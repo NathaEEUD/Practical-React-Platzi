9b587960c88ab5a3838aee74bd05dd10a38d663b
Nathaly <nriano@robotec.com>
2019-09-17 16:45:02 -0500

    feat: structure and styles of search, categories, carousel and carousel item sections

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import '../assets/styles/App.scss';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Categories from '../components/Categories';
 import Header from '../components/Header';
+import Search from '../components/Search';
 
 const App = () => (
   <div className="app">
     <Header />
+    <Search />
+    <Categories>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
   </div>
 );
 
